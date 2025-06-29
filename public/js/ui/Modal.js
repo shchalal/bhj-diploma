27cc4a43a416a790662023ffaf50cdd6bef5e18d
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-29 23:57:30 +0300

    no message

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,66 +1,40 @@
-
 class Modal {
- 
-  static instances = {};
-
   
   constructor(element) {
     if (!element) {
-      throw new Error('Не был передан элемент модального окна');
+      throw new Error('Элемент для модального окна не найден');
     }
     this.element = element;
-    this.id = element.id;
     this.registerEvents();
-    if (this.id) {
-      Modal.instances[this.id] = this;
-    }
-  }
-
-  registerEvents() {
-    const dismissButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
-    dismissButtons.forEach(button => {
-      button.addEventListener('click', e => this.onClose(e));
-    });
   }
 
  
+  registerEvents() {
+    const closeButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
+    closeButtons.forEach(btn => {
+      btn.addEventListener('click', this.onClose.bind(this));
+    });
+  }
+
   onClose(e) {
     e.preventDefault();
     this.close();
   }
 
- 
+  
   open() {
     this.element.style.display = 'block';
-  
-    this.element.offsetWidth;
-    this.element.classList.add('in');
   }
 
-  
+
   close() {
-    this.element.classList.remove('in');
-    const removeDisplay = () => this.element.style.removeProperty('display');
-    this.element.addEventListener('transitionend', removeDisplay, { once: true });
-    setTimeout(removeDisplay, 300);
+    this.element.style.removeProperty('display');
   }
+} 
 
- 
-  static initAll() {
-   
-    document.querySelectorAll('.modal').forEach(el => new Modal(el));
-  
-    document.querySelectorAll('[data-toggle="modal"]').forEach(btn => {
-      btn.addEventListener('click', e => {
-        e.preventDefault();
-        const target = btn.getAttribute('data-target');
-        if (!target) return;
-        const id = target.startsWith('#') ? target.slice(1) : target;
-        const modal = Modal.instances[id];
-        if (modal) modal.open();
-      });
-    });
-  }
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = Modal;
+} else {
+  window.Modal = Modal;
 }
-
-Modal.initAll();
