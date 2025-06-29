150906d4a4e41596919f8d96530c2e69938a67c3
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-29 22:40:44 +0300

    no message

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,34 +1,29 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
-class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
+export class Sidebar {
+ 
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
+ 
   static initToggleButton() {
+    const toggleButton = document.querySelector('.sidebar-toggle.visible-xs');
+    if (!toggleButton) return;
 
+    toggleButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.body.classList.toggle('sidebar-open');
+      document.body.classList.toggle('sidebar-collapse');
+    });
   }
 
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
+  
   static initAuthLinks() {
-
+   
   }
 }
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  Sidebar.init();
+});
