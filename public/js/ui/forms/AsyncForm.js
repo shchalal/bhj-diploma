56a78b14e211faa940025b1eacd6cfa4997e6684
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-29 23:24:40 +0300

    no message

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -1,49 +1,41 @@
-/**
- * Класс AsyncForm управляет всеми формами
- * приложения, которые не должны быть отправлены с
- * перезагрузкой страницы. Вместо этого данные
- * с таких форм собираются и передаются в метод onSubmit
- * для последующей обработки
- * */
 class AsyncForm {
-  /**
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * Сохраняет переданный элемент и регистрирует события
-   * через registerEvents()
-   * */
+
   constructor(element) {
-
+    if (!element || element.tagName !== 'FORM') {
+      throw new Error('AsyncForm: передан неверный элемент');
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
-  /**
-   * Необходимо запретить отправку формы и в момент отправки
-   * вызывает метод submit()
-   * */
+ 
   registerEvents() {
-
+    this.element.addEventListener('submit', event => {
+      event.preventDefault();
+      this.submit();
+    });
   }
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
   getData() {
-
+    const formData = new FormData(this.element);
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
+    }
+    return data;
   }
 
-  onSubmit(options){
-
+  
+  onSubmit(formData) {
+   
   }
 
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
+ 
   submit() {
-
+    const data = this.getData();
+    this.onSubmit(data);
   }
 }
+
+
+window.AsyncForm = AsyncForm;
