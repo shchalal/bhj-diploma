6a384ade96b94e5add7738acb5df2ec977d89374
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-29 23:29:33 +0300

    no message

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,35 +1,61 @@
-class Modal {
- 
-  constructor(element) {
-    if (!element) {
-      throw new Error('Modal: передан пустой или неопределённый элемент');
+(function() {
+  class Modal {
+
+    constructor(element) {
+      if (!(element instanceof HTMLElement)) {
+        throw new Error('Modal: передан неверный элемент');
+      }
+      this.element = element;
+
+    
+      this.onCloseClick = this.onCloseClick.bind(this);
+      this.onBackdropClick = this.onBackdropClick.bind(this);
+      this.onEscPress     = this.onEscPress.bind(this);
+
+      this.registerEvents();
     }
-    this.element = element;
-    this.registerEvents();
-  }
+
+   
+    registerEvents() {
+     
+      this.element
+        .querySelectorAll('[data-dismiss="modal"]')
+        .forEach(btn => btn.addEventListener('click', this.onCloseClick));
+
+     
+      this.element.addEventListener('click', this.onBackdropClick);
+    }
 
   
-  registerEvents() {
-    const closeButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
-    closeButtons.forEach(button => {
-      button.addEventListener('click', this.onClose.bind(this));
-    });
-  }
+    onCloseClick(e) {
+      e.preventDefault();
+      this.close();
+    }
 
   
-  onClose(e) {
-    e.preventDefault();      
-    this.close();
+    onBackdropClick(e) {
+      if (e.target === this.element) {
+        this.close();
+      }
+    }
+
+   
+    open() {
+      this.element.style.display = 'block';
+      document.addEventListener('keydown', this.onEscPress);
+    }
+
+    close() {
+      this.element.style.display = 'none';
+      document.removeEventListener('keydown', this.onEscPress);
+    }
+
+    onEscPress(e) {
+      if (e.key === 'Escape' || e.key === 'Esc') {
+        this.close();
+      }
+    }
   }
 
-  open() {
-    this.element.style.display = 'block';
-  }
-
-  
-  close() {
-    this.element.style.removeProperty('display');
-  }
-}
-
-window.Modal = Modal;
+  window.Modal = Modal;
+})();
