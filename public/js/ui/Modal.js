12c3d51fe9c73f78d3f2ae86606bc32ce6781d76
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-30 00:03:28 +0300

    no message

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,36 +1,38 @@
-class Modal {
 
+class Modal {
+ 
   constructor(element) {
     if (!element) {
       throw new Error('Элемент для модального окна не найден');
     }
     this.element = element;
     this.registerEvents();
+    
+    this.close();
   }
 
+ 
   registerEvents() {
-   
     this.element.addEventListener('click', event => {
-  
-      if (event.target.closest('[data-dismiss="modal"]')) {
+      const closeTrigger = event.target.closest('[data-dismiss="modal"]');
+      if (closeTrigger) {
         this.onClose(event);
       }
     });
   }
 
-
+  
   onClose(e) {
     e.preventDefault();
     this.close();
   }
-
 
   open() {
     this.element.style.display = 'block';
     this.element.classList.add('in');
   }
 
- 
+
   close() {
     this.element.style.removeProperty('display');
     this.element.classList.remove('in');
