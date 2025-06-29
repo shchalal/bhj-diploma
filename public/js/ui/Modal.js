8d62e490aa2bb835ee2f1307840a4d9d64eae1df
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-06-29 23:48:42 +0300

    no message

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,16 +1,21 @@
 
 class Modal {
  
+  static instances = {};
+
+  
   constructor(element) {
     if (!element) {
       throw new Error('Не был передан элемент модального окна');
     }
-
     this.element = element;
+    this.id = element.id;
     this.registerEvents();
+    if (this.id) {
+      Modal.instances[this.id] = this;
+    }
   }
 
-  
   registerEvents() {
     const dismissButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
     dismissButtons.forEach(button => {
@@ -20,25 +25,42 @@ class Modal {
 
  
   onClose(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     this.close();
   }
 
  
   open() {
     this.element.style.display = 'block';
+  
     this.element.offsetWidth;
     this.element.classList.add('in');
   }
 
-
+  
   close() {
     this.element.classList.remove('in');
     const removeDisplay = () => this.element.style.removeProperty('display');
     this.element.addEventListener('transitionend', removeDisplay, { once: true });
     setTimeout(removeDisplay, 300);
   }
+
+ 
+  static initAll() {
+   
+    document.querySelectorAll('.modal').forEach(el => new Modal(el));
+  
+    document.querySelectorAll('[data-toggle="modal"]').forEach(btn => {
+      btn.addEventListener('click', e => {
+        e.preventDefault();
+        const target = btn.getAttribute('data-target');
+        if (!target) return;
+        const id = target.startsWith('#') ? target.slice(1) : target;
+        const modal = Modal.instances[id];
+        if (modal) modal.open();
+      });
+    });
+  }
 }
 
-
-export default Modal;
+Modal.initAll();
